b5de8e707be428081f10e108b306097816d36041
Melis Meriç <37816087+melismeric@users.noreply.github.com>
2023-11-06 16:03:40 +0000

    add loading text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,10 @@ import ImageViewer from './components/ImageViewer';
 
 function App() {
   const [imageUrl, setImageUrl] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const generateImage = async (prompt) => {
+    setIsLoading(true); // Set loading state to true while fetching the image
     try {
       const response = await fetch(`http://localhost:5000/generate?prompt=${prompt}`);
       const blob = await response.blob();
@@ -14,6 +16,8 @@ function App() {
       setImageUrl(url);
     } catch (error) {
       console.error('Error generating image:', error);
+    } finally {
+      setIsLoading(false); // Set loading state to false after fetching the image
     }
   };
 
@@ -21,7 +25,8 @@ function App() {
     <div className="App">
       <h1>Image Generator</h1>
       <ImageGeneratorForm generateImage={generateImage} />
-      {imageUrl && <ImageViewer imageUrl={imageUrl} />}
+      {isLoading && <p>Loading...</p>}
+      {imageUrl && !isLoading && <ImageViewer imageUrl={imageUrl} />}
     </div>
   );
 }
